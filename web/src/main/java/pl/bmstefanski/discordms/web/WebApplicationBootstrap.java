886be88908362029a1c5ecc6029eaca21566d4ff
unknown <bstefanskicom@gmail.com>
2018-10-09 22:59:37 +0200

    Set up thymeleaf and h2 embedded database

---
+++ web/src/main/java/pl/bmstefanski/discordms/web/WebApplicationBootstrap.java
@@ -0,0 +1,14 @@
+package pl.bmstefanski.discordms.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebApplicationBootstrap {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplicationBootstrap.class, args);
+  }
+
+}
