0ced20dd92770e7250ab1a591a0043953299b385
unknown <bstefanskicom@gmail.com>
2018-10-26 16:08:23 +0200

    Fix continuous integration services

---
+++ bot/src/main/java/pl/bmstefanski/discordms/bot/BotApplicationBootstrap.java
@@ -0,0 +1,13 @@
+package pl.bmstefanski.discordms.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotApplicationBootstrap {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BotApplicationBootstrap.class, args);
+  }
+
+}
