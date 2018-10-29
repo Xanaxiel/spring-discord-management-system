a48472542d6c11f2ded98d4f14e21eb76e689037
unknown <bstefanskicom@gmail.com>
2018-10-29 20:36:22 +0100

    Add file storage service

--- web/src/main/java/pl/bmstefanski/discordms/web/service/UserService.java
+++ web/src/main/java/pl/bmstefanski/discordms/web/service/UserService.java
@@ -1,9 +1,12 @@
 package pl.bmstefanski.discordms.web.service;
 
 import pl.bmstefanski.discordms.web.entity.user.UserEntity;
+import pl.bmstefanski.discordms.web.form.ProfileDetailsForm;
 
 public interface UserService {
 
   void saveUser(UserEntity userEntity);
+
+  void convertProfileDetailsToUserEntity(UserEntity userEntity, ProfileDetailsForm form);
 
 }
--- web/src/main/java/pl/bmstefanski/discordms/web/service/impl/UserServiceImpl.java
+++ web/src/main/java/pl/bmstefanski/discordms/web/service/impl/UserServiceImpl.java
@@ -1,25 +1,45 @@
 package pl.bmstefanski.discordms.web.service.impl;
 
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.bmstefanski.discordms.web.entity.user.UserEntity;
 import pl.bmstefanski.discordms.web.entity.user.UserEntityImpl;
+import pl.bmstefanski.discordms.web.form.ProfileDetailsForm;
 import pl.bmstefanski.discordms.web.repository.UserRepository;
+import pl.bmstefanski.discordms.web.service.FileStorageService;
 import pl.bmstefanski.discordms.web.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final FileStorageService fileStorageService;
 
   @Autowired
-  public UserServiceImpl(UserRepository userRepository) {
+  public UserServiceImpl(UserRepository userRepository, FileStorageService fileStorageService) {
     this.userRepository = userRepository;
+    this.fileStorageService = fileStorageService;
   }
 
   @Override
   public void saveUser(UserEntity userEntity) {
     this.userRepository.save((UserEntityImpl) userEntity);
+  }
+
+  @Override
+  public void convertProfileDetailsToUserEntity(UserEntity userEntity, ProfileDetailsForm form) {
+    userEntity.setFirstName(this.validateFormField(userEntity.getFirstName()));
+    userEntity.setSecondName(this.validateFormField(userEntity.getSecondName()));
+    userEntity.setDescription(this.validateFormField(userEntity.getDescription()));
+    userEntity.setBirthday(form.getBirthday() == null ? LocalDate.now() : form.getBirthday());
+    userEntity.setBannerUrl(this.fileStorageService.getFileUrl(form.getBanner()));
+
+    this.saveUser(userEntity);
+  }
+
+  private String validateFormField(String value) {
+    return value.isEmpty() ? "N/A" : value;
   }
 
 }
